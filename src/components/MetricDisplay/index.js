8cc513f6ee0439ad047653402e303c057f5b7efe
Cristi Scheye <cristi.scheye@gmail.com>
2016-11-11 18:01:09 -0800

    misc cleanup MetricDisplay

--- src/components/MetricDisplay/index.js
+++ src/components/MetricDisplay/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import Eye from 'react-icons/lib/go/eye';
 import Star from 'react-icons/lib/go/star';
 import RepoForked from 'react-icons/lib/go/repo-forked';
@@ -12,11 +12,12 @@ const icons = {
 
 class MetricDisplay extends Component {
   static defaultProps = {
+    icon: 'eye',
     value: 0
   };
 
   static propTypes = {
-    icon: React.PropTypes.oneOf(['eye', 'star', 'fork']),
+    icon: PropTypes.oneOf(['eye', 'star', 'fork']),
     value: 0
   };
 
